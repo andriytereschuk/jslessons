40ad3aabb58f184753d0fc8084f5c53933e88161
andrii.tereshchuk <andrii.tereshchuk@eleks.com>
2014-12-09 19:34:10 +0200

    gallery: done slide effect and sync with thumb

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -8,41 +8,47 @@ var options = {
 $.fn.gallery = function(aOptions) {
 	var self = $(this);
 	var gallery = {};
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();	
+	var galleryWidth = $('.gallery').width();
+	var currentSlide = 0,
+			slideCount = 0;
 
-	/* Initializes namespace settings to be used throughout plugin */
 	var init = function() {
 		gallery.settings = $.extend(options, aOptions);
 
-		// perform all DOM / CSS modifications
+		// perform all DOM
 		setup();
 	}
 
-	/* Perform all DOM / CSS modifications*/
+	/* Perform all DOM */
 	var setup = function() {
 		// create img and thumbnail wrapper
-		self.append('<div class="gallery-img-wrapper"><a class="gallery-arrows gallery-left"></a><a class="gallery-arrows gallery-right"></a><ul class="gallery-img"></ul></div><div class="thumbnail-img-wrapper"><ul class="thumbnail-img"></ul></div>');
+		self.append('<div class="gallery-img-wrapper"><a class="gallery-arrows gallery-left"></a><a class="gallery-arrows gallery-right"></a>'+''
+			+'<ul class="gallery-img"></ul></div><div class="thumbnail-img-wrapper"><ul class="thumbnail-img"></ul></div>');
+
+		wrap = $('.gallery-img-wrapper');
+		ulImg = $('.gallery-img');
+		ulThumb = $('.thumbnail-img');
 
 		// get img via AJAX
 		$.ajax({
 			type: 'GET',
 			url: 'api/gallery.json',
-			crossDomain: true,
 			dataType: 'json',
 			success: function(response) {
-				for (var i = 0; i < response.images.length; i++) {
-					$('.gallery-img')
+				slideCount  = response.images.length;
+				for (var i = 0; i < slideCount; i++) {
+					ulImg
 						.append('<li><a target="_blank" href=' + response.images[i].url +
-						 '><img data-id=img' +i+ ' src=' + response.images[i].imageURL + '></a></li>');
-					$('.thumbnail-img')
+						 '><img data-id=img' + i + ' src=' + response.images[i].imageURL + '></a></li>');
+					ulThumb
 						.append('<li><img src=' + response.images[i].thumbnailURL + '></li>');
 				}
 				
 				// set width for element
-				$('.gallery-img > li').css('width',windowWidth);
+				ulImg.children('li').css('width', galleryWidth);
 
 				getImageHeight();
+				thumb();
 			}
 		});
 	}
@@ -51,6 +57,7 @@ $.fn.gallery = function(aOptions) {
 		$('img[data-id="img0"]').one('load', function() {
 			var firstImageHeight = this.height;
 			$('.gallery-img-wrapper').height(firstImageHeight);
+			$(this).closest('li').addClass('active');
 
 			if(gallery.settings.effect == 'slide') {
 				slide();
@@ -59,16 +66,85 @@ $.fn.gallery = function(aOptions) {
 	}
 
 	var slide = function() {
-		$('.gallery-arrows').on({
+		// click left
+		$('.gallery-left').on({
 			click: function(event) {
 				event.preventDefault();
-				var value  = -windowWidth;
-				var heightNextImage = $('img[data-id="img1"]').height();
-				$('.gallery-img').css('transform','translate3d(' + value + 'px, 0, 0)');
-				$('.gallery-img-wrapper').height(heightNextImage);
+				// show right button
+				$('.gallery-right').show();
+				--currentSlide;
+				if (!currentSlide) $(this).hide();
+				slideChange();
+			}
+		});
+
+		// click right
+		$('.gallery-right').on({
+			click: function(event) {
+				event.preventDefault();
+				// show right button
+				$('.gallery-left').show();
+				++currentSlide;
+				if(currentSlide == slideCount - 1) $(this).hide();
+				slideChange();
 			}
 		});
 	}
+
+
+	var slideChange = function () {
+		// change active slide 
+		ulImg.children('li')
+			.removeClass('active')
+			.eq(currentSlide)
+			.addClass('active');
+
+		// calc slide value
+		var slideWidth = wrap.width();
+		var slideValue  = -slideWidth * currentSlide;
+		ulImg.css('transform','translate3d(' + slideValue + 'px, 0, 0)');
+
+		// calc slider wrap
+		var heightNextImage = $('img[data-id="img' + currentSlide + '"]').height();				
+		wrap.height(heightNextImage);
+
+		syncThumb();
+	}
+
+	var thumb = function() {
+		ulThumb.children('li').first().addClass('active');
+
+		// click thumb img
+		ulThumb.children('li').on({
+			click: function(event) {
+				event.preventDefault();
+
+				currentSlide = $(this).index();								
+				if(currentSlide == slideCount - 1) {
+					$('.gallery-right').hide();
+					$('.gallery-left').show();
+				}
+				if(!currentSlide) {
+					$('.gallery-left').hide();
+					$('.gallery-right').show();
+				};
+
+				if(currentSlide > 0 && currentSlide < (slideCount-1)) {
+					$('.gallery-left').show();
+					$('.gallery-right').show();					
+				}
+
+				slideChange();
+			}
+		});		
+	}
+
+	var syncThumb = function() {
+		ulThumb.children('li')
+			.removeClass('active')
+			.eq(currentSlide)
+			.addClass('active');
+	}	
 
 	init();
 	return this;
